aea0c2a3682f01ed5182c2827282ee01f4d15d6e
shadiyarov <sukhrobshadiyarov@Sukhrobs-MBP.home>
2020-07-11 11:46:13 -0400

    arrays

--- lessons/2-lesson/index.php
+++ lessons/2-lesson/index.php
@@ -1,7 +1,18 @@
 <?php
-//$clubs = ['Manchester United','Liverpool','Real Madrid','Barcelona','Inter Milan','Juventus'];
-$cities = ['United States'=>'New York','New York'=>'Brooklyn',77,'California'=>'Hollywood'];
-echo "<pre>";
-print_r($cities);
+$clubs = [
+    'Manchester United' => ['David De Gea','Bruno Fernandes','Paul Pogba','Daniel James'],
+    'Liverpool' => ['Yurgen Klopp'=>['Borussia Dortmung']],
+    'Real Madrid',
+    'Barcelona',
+    'Inter Milan',
+    'Juventus'
+];
 
+unset($clubs['2']);
+
+$clubs[2] = 'Arsenal';
+echo "<pre>";
+//print_r($clubs['Manchester United'][1]);//Bruno Fernandes
+//print_r($clubs['Liverpool']['Yurgen Klopp']['0']);//Borussia Dortmund
+print_r($clubs);//unset removed Inter Milan and Arsenal added
 
