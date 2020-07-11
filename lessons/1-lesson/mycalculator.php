9998cb770113a2f048d54c11cdf7072a7e6acbaf
shadiyarov <sukhrobshadiyarov@Sukhrobs-MBP.home>
2020-07-11 11:22:23 -0400

    arrays

--- lessons/1-lesson/mycalculator.php
+++ lessons/1-lesson/mycalculator.php
@@ -6,7 +6,7 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Bathic Math Operation</title>
-    <link rel="stylesheet" href="../css/bootstrap.css">
+    <link rel="stylesheet" href="../../css/bootstrap.css">
 </head>
 <body>
 <div class="result">
---
+++ lessons/2-lesson/index.php
@@ -0,0 +1,7 @@
+<?php
+//$clubs = ['Manchester United','Liverpool','Real Madrid','Barcelona','Inter Milan','Juventus'];
+$cities = ['United States'=>'New York','New York'=>'Brooklyn',77,'California'=>'Hollywood'];
+echo "<pre>";
+print_r($cities);
+
+
