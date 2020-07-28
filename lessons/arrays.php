70985ea1aeb45256db49d2e0b31f724a6d9f6567
shadiyarov <sukhrobshadiyarov@Sukhrobs-MBP.home>
2020-07-28 10:58:24 -0400

    array_column

--- lessons/arrays.php
+++ lessons/arrays.php
@@ -1,7 +1,6 @@
 <?php
 //Arrays exercises
-
-$phones = [
+/*$phones = [
     'iphone'=>'iphone_x_max',
     'samsung'=>'gallaxy_9+',
     'nokia',
@@ -15,5 +14,37 @@ $countries = [
     'Uzbekistan'=>'Samarkand'
 ];
 
+//razr_v3 - new_york - javobi shunaqa chiqishi kk. Senga darsgacha vazifa
+*/
+
+/*$football_players = [
+    'Ferdandes','Rashford','Fred','Ronaldo','Messi','Felix'
+];
+$key = array_search('Ronaldo',$football_players);
+print_r($football_players[$key]);
+*/
+//Array-column
+
+$countries = [
+    'Uzbekistan'=>[
+        'capital'=>'Tashkent',
+        'language'=>'uzbek',
+        'religion'=>'muslim'
+    ],
+
+    'Russia'=>[
+    'capital'=>'Moscow',
+    'language'=>'russian',
+    'religion'=>'christian'
+    ],
+
+    'USA'=>[
+        'capital'=>'Vashington DC',
+        'language'=>'american-english',
+        'religion'=>'christian'
+    ],
+];
+
+$result = array_column($countries,'capital');
 echo "<pre>";
-print_r($phones['motorola']. ' - ' .$countries['US']);
+print_r($result);
